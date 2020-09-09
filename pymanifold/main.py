9a1ff5446bed806434edd25a88b8ba1008ba1232
ejshim@imagoworks.ai <soulrommel@gmail.com>
2020-09-09 18:02:33 +0900

    update

--- pymanifold/main.py
+++ pymanifold/main.py
@@ -7,6 +7,7 @@ iren = vtk.vtkRenderWindowInteractor()
 iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
 
 renWin = vtk.vtkRenderWindow()
+renWin.SetSize(1000, 1000)
 iren.SetRenderWindow(renWin)
 
 ren = vtk.vtkRenderer()
@@ -21,11 +22,11 @@ def MakeActor(polydata):
     return actor
 
 
-library_path = os.path.dirname(os.path.abspath(__file__))
-
-lib = cdll.LoadLibrary(os.path.join(library_path, "../build/manifold_lib"))
+library_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+lib = cdll.LoadLibrary(os.path.join(library_path, "build", "manifold_lib"))
 
 lib.Calculate.restype = py_object
+lib.Simplify.restype = py_object
 
 def CalculateManifold(polydata, resolution = 1000):
 
@@ -80,11 +81,67 @@ def CalculateManifold(polydata, resolution = 1000):
     return result
 
 
+def Simplify(polydata, max_faces):
+    
+    
+    #Get Vertex and Face Array
+    nPoints = polydata.GetNumberOfPoints()
+    nFaces = polydata.GetNumberOfCells()
+
+    v = []
+    f = []
+
+    for i in range(nPoints):
+        v.append(polydata.GetPoint(i))
+
+    for i in range(nFaces):
+        cell = polydata.GetCell(i)
+
+        cell.GetPointId(0)
+
+        f.append([cell.GetPointId(0), cell.GetPointId(1), cell.GetPointId(2) ])
+
+    v = np.array(v)
+    f = np.array(f)
+
+
+    calculated = lib.Simplify(v.ctypes.data_as(POINTER(c_double)), v.size,
+                    f.ctypes.data_as(POINTER(c_int)), f.size, c_int(max_faces))
+
+
+    vertices = np.array( calculated['vertices'])
+    vertices = vertices.reshape(int(vertices.size/3), 3)
+    faces = np.array( calculated['faces'])
+    faces = faces.reshape(int(faces.size/3), 3)
+
+
+    result = vtk.vtkPolyData()
+
+    points = vtk.vtkPoints()
+    points.SetNumberOfPoints(vertices.shape[0])
+    for i in range(vertices.shape[0]):
+        points.SetPoint(i, vertices[i])
+    result.SetPoints(points)
+
+    cells = vtk.vtkCellArray()
+    for i in range(faces.shape[0]):
+        cell = vtk.vtkTriangle()
+        cell.GetPointIds().SetId(0, faces[i][0])
+        cell.GetPointIds().SetId(1, faces[i][1])
+        cell.GetPointIds().SetId(2, faces[i][2])
+        cells.InsertNextCell(cell)
+    result.SetPolys(cells)
+
+    return result
+
+
 def CalculateOBJ(path):
     lib.CalculateOBJ(path)
         
 if __name__ == "__main__":
     data_path = "../exp/sampleData/mandibular_nonmanifold.obj"    
+
+
 
 
     reader = vtk.vtkOBJReader()
@@ -93,13 +150,15 @@ if __name__ == "__main__":
 
     polydata = reader.GetOutput()
 
+    manifold = CalculateManifold(polydata, 2000)
 
-    #Calculate Manifold, Save OBJ
-    manifold = CalculateManifold(polydata, 100)
+    polydata = Simplify(manifold, 3000)
 
 
+
+    
     #After Calculating
-    actor = MakeActor(manifold)
+    actor = MakeActor(polydata)
     ren.AddActor(actor)
 
     renWin.Render()
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include "Model_OBJ.h"
 #include <Python.h>
 
-
+//simplify
+#include "igl/decimate.h"
+#include "igl/qslim.h"
 class Foo{
 public:
     void bar(){
@@ -69,6 +71,191 @@ extern "C" {
         PyDict_SetItem(dict, Py_BuildValue("s", "faces"), fList);
 
         return dict;
+    }
+
+
+    __declspec(dllexport) PyObject* Simplify(double* posPtr, int posSize, int* facePtr, int faceSize, int max_faces=0x7fffffff){
+
+          
+        PyObject* dict = PyDict_New();
+
+        using namespace std;
+        using namespace Eigen;
+        using namespace igl;
+      
+        double max_cost = 1e30;
+        double max_ratio = 2.;
+        bool check_manifold = false;
+
+        MatrixXd OV = Map<MatrixXd>(posPtr,  3, posSize/3).transpose();
+        MatrixXi OF = Map<MatrixXi>(facePtr, 3, faceSize/3).transpose();
+
+        if (max_faces == 0x7fffffff) {
+            max_faces = max_ratio * OF.rows() + 0.5;
+        } else {
+        if (max_ratio < 1)
+            max_faces = std::max(max_faces, (int)(max_ratio * OF.rows() + 0.5));
+        }
+        
+        std::cout << "max faces : " << max_faces << std::endl;
+
+        auto MyDecimate = [&](
+            const Eigen::MatrixXd & V,
+            const Eigen::MatrixXi & F,
+            const size_t max_m,
+            const double max_cost,
+            Eigen::MatrixXd & U,
+            Eigen::MatrixXi & G,
+            Eigen::VectorXi & J,
+            Eigen::VectorXi & I) {
+            // Original number of faces
+            const int orig_m = F.rows();
+            // Tracking number of faces
+            int m = F.rows();
+            typedef Eigen::MatrixXd DerivedV;
+            typedef Eigen::MatrixXi DerivedF;
+            DerivedV VO;
+            DerivedF FO;
+            igl::connect_boundary_to_infinity(V,F,VO,FO);
+            // decimate will not work correctly on non-edge-manifold meshes. By extension
+            // this includes meshes with non-manifold vertices on the boundary since these
+            // will create a non-manifold edge when connected to infinity.
+            if(!is_edge_manifold(FO))
+            {
+              return false;
+            }
+        
+            auto stopping_condition = 
+            [&](
+            const Eigen::MatrixXd &V,
+            const Eigen::MatrixXi &F,
+            const Eigen::MatrixXi &E,
+            const Eigen::VectorXi &EMAP,
+            const Eigen::MatrixXi &EF,
+            const Eigen::MatrixXi &EI,
+            const std::set<std::pair<double,int> > &Q,
+            const std::vector<std::set<std::pair<double,int> >::iterator > &QIT,
+            const Eigen::MatrixXd &C,
+            const int e,
+            const int e1,
+            const int e2,
+            const int f1,
+            const int f2)->bool
+            {
+              // Only subtract if we're collapsing a real face
+              if(f1 < orig_m) m-=1;
+              if(f2 < orig_m) m-=1;
+              return (m<=(int)max_m) || (Q.begin()->first >= max_cost);
+            };
+        
+            using namespace igl;
+        
+            Eigen::VectorXi EMAP;
+            Eigen::MatrixXi E,EF,EI;
+            edge_flaps(FO,E,EMAP,EF,EI);
+            // Quadrics per vertex
+            typedef std::tuple<Eigen::MatrixXd,Eigen::RowVectorXd,double> Quadric;
+            std::vector<Quadric> quadrics;
+            per_vertex_point_to_plane_quadrics(VO,FO,EMAP,EF,EI,quadrics);
+            // State variables keeping track of edge we just collapsed
+            int v1 = -1;
+            int v2 = -1;
+            // Callbacks for computing and updating metric
+            std::function<void(
+              const int e,
+              const Eigen::MatrixXd &,
+              const Eigen::MatrixXi &,
+              const Eigen::MatrixXi &,
+              const Eigen::VectorXi &,
+              const Eigen::MatrixXi &,
+              const Eigen::MatrixXi &,
+              double &,
+              Eigen::RowVectorXd &)> cost_and_placement;
+            std::function<bool(
+              const Eigen::MatrixXd &                                         ,/*V*/
+              const Eigen::MatrixXi &                                         ,/*F*/
+              const Eigen::MatrixXi &                                         ,/*E*/
+              const Eigen::VectorXi &                                         ,/*EMAP*/
+              const Eigen::MatrixXi &                                         ,/*EF*/
+              const Eigen::MatrixXi &                                         ,/*EI*/
+              const std::set<std::pair<double,int> > &                        ,/*Q*/
+              const std::vector<std::set<std::pair<double,int> >::iterator > &,/*Qit*/
+              const Eigen::MatrixXd &                                         ,/*C*/
+              const int                                                        /*e*/
+              )> pre_collapse;
+            std::function<void(
+              const Eigen::MatrixXd &                                         ,   /*V*/
+              const Eigen::MatrixXi &                                         ,   /*F*/
+              const Eigen::MatrixXi &                                         ,   /*E*/
+              const Eigen::VectorXi &                                         ,/*EMAP*/
+              const Eigen::MatrixXi &                                         ,  /*EF*/
+              const Eigen::MatrixXi &                                         ,  /*EI*/
+              const std::set<std::pair<double,int> > &                        ,   /*Q*/
+              const std::vector<std::set<std::pair<double,int> >::iterator > &, /*Qit*/
+              const Eigen::MatrixXd &                                         ,   /*C*/
+              const int                                                       ,   /*e*/
+              const int                                                       ,  /*e1*/
+              const int                                                       ,  /*e2*/
+              const int                                                       ,  /*f1*/
+              const int                                                       ,  /*f2*/
+              const bool                                                  /*collapsed*/
+              )> post_collapse;
+            qslim_optimal_collapse_edge_callbacks(
+              E,quadrics,v1,v2, cost_and_placement, pre_collapse,post_collapse);
+            // Call to greedy decimator
+            bool ret = decimate(
+              VO, FO,
+              cost_and_placement,
+              stopping_condition,
+              pre_collapse,
+              post_collapse,
+              E, EMAP, EF, EI,
+              U, G, J, I);
+            // Remove phony boundary faces and clean up
+            const Eigen::Array<bool,Eigen::Dynamic,1> keep = (J.array()<orig_m);
+            igl::slice_mask(Eigen::MatrixXi(G),keep,1,G);
+            igl::slice_mask(Eigen::VectorXi(J),keep,1,J);
+            Eigen::VectorXi _1,I2;
+            igl::remove_unreferenced(Eigen::MatrixXd(U),Eigen::MatrixXi(G),U,G,_1,I2);
+            igl::slice(Eigen::VectorXi(I),I2,1,I);
+        
+            return ret;
+          };
+
+          MatrixXd V;
+          MatrixXi F;
+          VectorXi J, I;
+          MyDecimate(OV, OF, max_faces, max_cost, V, F, J, I);
+
+
+          std::cout << V.rows() << "," << V.cols() << std::endl;
+          std::cout << F.rows() << "," << F.cols() << std::endl;
+
+
+          write_triangle_mesh("test.obj",V,F);
+
+          PyObject *vList = PyList_New(0);
+          PyObject *fList = PyList_New(0);
+  
+          for(int i=0 ; i< V.rows() ; i++){
+              PyList_Append(vList, Py_BuildValue("d", V.row(i)[0]));
+              PyList_Append(vList, Py_BuildValue("d", V.row(i)[1]));
+              PyList_Append(vList, Py_BuildValue("d", V.row(i)[2]));
+          }
+  
+          for(int i=0 ; i< F.rows() ; i++){
+              PyList_Append(fList, Py_BuildValue("i", F.row(i)[0]));
+              PyList_Append(fList, Py_BuildValue("i", F.row(i)[1]));
+              PyList_Append(fList, Py_BuildValue("i", F.row(i)[2]));
+          }
+  
+
+          PyDict_SetItem(dict, Py_BuildValue("s", "vertices"), vList);
+          PyDict_SetItem(dict, Py_BuildValue("s", "faces"), fList);
+
+          return dict;
+  
+
     }
 };
 
